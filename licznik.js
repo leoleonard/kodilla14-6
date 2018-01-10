5e6d17f9dac14b2a3d09434380748ec6b6bbeed9
Damian Juzyk <damian.juzyk@gmail.com>
2018-01-10 21:38:35 +0400

    add zadanie 14-6

---
+++ licznik.js
@@ -0,0 +1,29 @@
+var Counter = React.createClass({
+
+    getInitialState: function() {
+    return {
+      counter: 0
+    };
+  },
+
+    decrement: function() {
+    this.setState({
+      counter: this.state.counter -1
+    });
+  },
+
+    increment: function () {
+      this.setState({
+        counter: this.state.counter +1
+      });
+    },
+
+
+    render: function() {
+      return React.createElement("div", {className: "container"},
+        React.createElement("span", {}, "Licznik: " + this.state.counter),
+        React.createElement("button", {onClick: this.increment}, "Plus"),
+        React.createElement("button", {onClick: this.decrement}, "Minus"),
+      );
+    }
+});
